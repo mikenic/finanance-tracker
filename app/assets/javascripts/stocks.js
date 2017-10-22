991efd28e03bea19f83407210a960cb7640d3305
mikenic <temperanceaubie@gmail.com>
2017-10-22 01:52:14 -0700

    add friends and search

--- app/assets/javascripts/stocks.js
+++ app/assets/javascripts/stocks.js
@@ -1,8 +1,6 @@
 var init_stock_lookup;
 
 init_stock_lookup = function() {
-
-
 	$('#stock-lookup-form').on('ajax:before', function(event, data, status){
 		show_spinner();
 	});
@@ -10,7 +8,6 @@ init_stock_lookup = function() {
 	$('#stock-lookup-form').on('ajax:after', function(event, data, status){
 		hide_spinner();
 	});
-
 
 	$('#stock-lookup-form').on('ajax:success', function(event, data, status){
 		$('#stock-lookup').replaceWith(data);
